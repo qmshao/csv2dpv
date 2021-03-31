b2bcf320db0962db3d08b9f8c9e31678843645d2
qmshao <qmshao@gmail.com>
2021-03-31 14:16:02 -0400

    remove debug

--- app/app.py
+++ app/app.py
@@ -136,4 +136,4 @@ def update_output(filename, contents):
 
 # Running the server
 if __name__ == "__main__":
-    app.run_server(debug=True, host='0.0.0.0', port=3800)
+    app.run_server(debug=False, host='0.0.0.0', port=3800)
